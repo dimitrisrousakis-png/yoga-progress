fe9353d08263b5711bcaa2df06a47eeaa741a571
dimitrisrousakis-png <dimitris.rousakis@gmail.com>
2026-09-13 21:00:45 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'yoga-progress-v9';
+const CACHE_NAME = 'yoga-progress-v13';
 const APP_SHELL = [
   './',
   './index.html',
@@ -25,6 +25,7 @@ self.addEventListener('activate', event => {
 
 self.addEventListener('fetch', event => {
   if (event.request.method !== 'GET') return;
+
   event.respondWith(
     fetch(event.request)
       .then(response => {
